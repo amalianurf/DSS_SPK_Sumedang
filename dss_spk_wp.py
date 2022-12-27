6372036204a4e9dff22df7162da63cfb9dbfc655
angls4 <57803800+angls4@users.noreply.github.com>
2022-12-27 22:03:25 +0700

    Comment sebagian

--- dss_spk_wp.py
+++ dss_spk_wp.py
@@ -13,62 +13,71 @@ import re
 
 # dict list = list berisi dictionary = [{key:value},{key:value}]
 # dict dict = dictionary berisi dictionary = {key:{key:value},key:{key:value}}
-# 
+# list list = list berisi list = [[value,value],[value,value]]
 
-# Response request URL jadi dict list
+# Response request URL berupa JSON jadi dict list
 def loadData(url):
   response =requests.get(url)
   data = response.json()
+  # ubah JSON list jadi dict list
   st.dataframe(data, use_container_width=True)
   return data
 
 # Ini fungsi-fungsi untuk ngumpulin data
 
 # Ambil values dari keys yang dipilih dalem dict
-def getDictValues(row,keys,is_keys_exception=False):
+def getDictValuesByKeys(row,keys,is_keys_exception=False):
+  # kalo keynya ada dalam keys, akan dimasukkan atau tidak tergantung is_keys_exception
   return [v for k,v in row.items() if (k in keys) != is_keys_exception]
 
 # Sum semua string angka dalam list
 def sumString(values:str):
   total=0
   for maybe_number in values:
-    # print(maybe_number)
     try:
       total += int(maybe_number)
+    # Handle untuk jika isi string adalah desimal
     except:
       total += float(maybe_number.replace(',','.'))
+    # Kalau isi string bukan angka return 0
   return total
 
-# {key_id:{+output_key:func(column_values)}} tambah pair ke dict dalem dict dari dict list diproses oleh func
+# Tambah pair ke dict dict dari dict list dengan value yang telah diproses oleh func
+# {id_key:{+(output_key:func(dict_value))}} 
 def ScrapColumnsFromList(output_dict,input_list,output_key,id_key,func,column_keys,is_column_keys_exception=False):
   for row in input_list:
+    # Dictionary output keynya di format
     target_dict = output_dict[formatKey(row[id_key])]
-    value = func(getDictValues(row,column_keys,is_column_keys_exception))
+    dict_value = getDictValuesByKeys(row,column_keys,is_column_keys_exception)
+    value = func(dict_value)
     if output_key in target_dict:
       target_dict[output_key] += value
     else:
       target_dict[output_key] = value
 
-# {key:{+output_key:func(column_values)}} tambah pair ke dict dalem dict dari dict dict diproses oleh func
+# Tambah pair ke dict dict dari dict dict dengan value yang telah diproses oleh func
+# {id_key:{+(output_key:func(dict_value))}}
 def ScrapColumnsFromDict(output_dict,input_dict,output_key,func,column_keys,is_column_keys_exception=False):
   for k,v in input_dict.items():
+    # Dictionary output keynya di format
     target_dict = output_dict[formatKey(k)]
-    value = func(getDictValues(v,column_keys,is_column_keys_exception))
+    dict_value = getDictValuesByKeys(v,column_keys,is_column_keys_exception)
+    value = func(dict_value)
     if output_key in target_dict:
       target_dict[output_key] += value
     else:
       target_dict[output_key] = value
 
-# {key:{-key:value}} remove key dari dict dict
-def removeKey(target:dict,key):
+# {key:{-(target_key:value)},key:{-(target_key:value)}} remove pair dari dict dalam dict dict berdasarkan target_key
+def removeKey(target:dict,target_key):
   for k,v in target.items():
-    v.pop(key)
+    v.pop(target_key)
 
 # "[010] Sumedang Utara" --> "SUMEDANG UTARA"
 def formatKey(input):
   return re.sub('[^a-zA-Z ]+', '', input).rstrip().lstrip().upper()
 
-# Buat yang bentuknya sama kyk di website Sumedang
+# Buat yang bentuknya sama kyk di website Sumedang (JSON list, tipe data : dict list)
 def dictDictToDictList(input:dict,key):
   output = []
   for k,v in input.items():
@@ -77,45 +86,55 @@ def dictDictToDictList(input:dict,key):
     output.append(row)
   return output
 
-#ini fungsi-fungsi dict list
-def getColumnsFromDictList(input:list,column_keys,is_column_keys_exception=False,func=None):
-  if func is None:
-    if len(column_keys) == 1 and is_column_keys_exception is False:
-      func = lambda x:x[0]
-    else:
-      func = lambda x:x
-  alt_table = []
-  for alt in input:
-    alt_table.append(func(getDictValues(alt,column_keys,is_column_keys_exception)))
-  return alt_table
 
-def addPairsToDictListFromList(input:list,key,values:list):
-  for i in range(0,len(input)):
-    row = input[i]
-    row.update({key:values[i]})
+#ini fungsi-fungsi dict list
+
+# Ambil semua value dari semua kolom yang dipilih (dalam bentuk list yang telah diproses func) dari dict list ke list list
+def getColumnsFromDictList(input:list,column_keys,is_column_keys_exception=False,func = lambda x:x):
+  output = []
+  for dict in input:
+    output.append(func(getDictValuesByKeys(dict,column_keys,is_column_keys_exception)))
+  return output
+
+# Tambahkan pairs dengan key yang ditentukan dan value dari list ke dict list
+# Values : [value1,value2] Target : [{+(key:value1)},{+(key:value2)}]
+def addPairsToDictListFromList(target:list,key,values:list):
+  for i in range(0,len(target)):
+    dict = target[i]
+    dict.update({key:values[i]})
+
 
 # Ini fungsi-fungsi WP
+
+# Return list berisi nilai-nilai input yang sudah dinormalisasi
 def normalisasi(weight_list,func = lambda x:x):
   weight_total = 0
   normalised_list = []
+  # Sum weight yang telah diproses oleh func
   for weight in weight_list:
     weight_total += func(weight)
   for weight in weight_list:
     normalised_list.append(weight/weight_total)
   return normalised_list
 
+# Buat vektor S dari list list value criteria setiap alternatif dan list bobot
 def buatVektorS(alt_table,weight_list):
   s = []
   for i in range(0,len(alt_table)):
-    si = 1
+    # si = nilai S sebuah alternatif
+    si = 1.0
+    # alt = list yang berisi nilai-nilai kriteria sebuah alternatif
     alt = alt_table[i]
     for j in range(0,len(alt)):
       try:
         criteria = int(alt[j])
+      # Handling jika nilai kriteria desimal
       except:
         criteria = float(alt[j])
+      # Handling jika nilai kriteria nol agar tidak ada pembagian oleh nol
       if criteria == 0:
         criteria = 1
+      # weight = weight untuk sebuah kriteria
       weight = weight_list[j]
       try:
         si *= pow(criteria,weight)
@@ -126,6 +145,8 @@ def buatVektorS(alt_table,weight_list):
 
 def wp(alternatives:list,weight_list,criterias,is_criterias_exception=False):
   output = []
+
+  # Deep copy dict list alternatives ke output
   for alt in alternatives:
     output.append(alt.copy())
 
@@ -202,6 +223,6 @@ with tab2:
     hasil_akhir = wp(alts,weight_list,excepted_columns,True)
     st.subheader('Hasil Akhir')
     st.dataframe(hasil_akhir, use_container_width=True)
-    dt = pd.DataFrame(hasil_akhir,index=getColumnsFromDictList(hasil_akhir,['kecamatan']),columns=['V'])
+    dt = pd.DataFrame(hasil_akhir,index=getColumnsFromDictList(hasil_akhir,['kecamatan'],func = lambda x:x[0]),columns=['V'])
     dt.sort_values(by=['V'],ascending=True)
     st.bar_chart(dt,width=1,)
